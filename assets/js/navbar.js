5043689260c369cfd0080ce277fe8cb1869947ea
00095417 <00095417@uca.edu.sv>
2018-10-19 13:31:18 -0600

    primer comentario

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,3 +1,23 @@
 /**
  * Code
  */
+
+ window.addEventListener('load',main);
+
+ function main(){
+    var flag = false;
+    var itemBtn = document.getElementById('btn-menu');
+    var itemNav = document.getElementsByTagName('body')[1];
+
+    itemBtn.addEventListener('click',() => {
+        console.log("asdasdasd");
+        if(flag){
+            flag = false;
+            itemNav.style.marginLeft = "0%";
+        }else{
+            flag = true;
+            itemNav.style.marginLeft = "-100%";
+        }
+    },false);
+    
+}
